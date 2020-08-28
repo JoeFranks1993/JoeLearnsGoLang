0d60f457e3046c98001045e8939cb2d9bef9f27c
JoeFranks1993 <Josephdeanfranks@gmail.com>
2020-08-28 16:06:38 -0500

    Editing comments to say 'slice' instead of 'splice'

--- SliceExample/SliceExample.go
+++ SliceExample/SliceExample.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	// Create Splice.
-	var list []int; // Or s := make([]string, 3) to create it with a default length.
-	
-	// Append an element to the splice.
-	list = append(list, 10);
-	list = append(list, 3);
-	list = append(list, 21);
-	list = append(list, 67);
+	// Create Slice.
+	var list []int // Or s := make([]string, 3) to create it with a default length.
 
-	// Append multiple items to spice with one invokation.
-	list = append(list, 21, 80, 56, 21);
+	// Append an element to the slice.
+	list = append(list, 10)
+	list = append(list, 3)
+	list = append(list, 21)
+	list = append(list, 67)
 
-	// Loop through the slice and print each elements. 
+	// Append multiple items to slice with one invokation.
+	list = append(list, 21, 80, 56, 21)
+
+	// Loop through the slice and print each elements.
 	for i := 0; i < len(list); i++ {
-		fmt.Println(list[i]);
+		fmt.Println(list[i])
 	}
 }
