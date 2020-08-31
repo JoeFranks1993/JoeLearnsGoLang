33f29c27424e01b2e0713297dd7f314b27050907
Joseph Franks <Josephdeanfranks@gmail.com>
2020-08-31 17:24:22 -0500

    Adding simple hashtable example

--- MapExample/MapExample.go
+++ MapExample/MapExample.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+var hashMap = make(map[string]int)
+
+func main() {
+	addValuesToMap()
+	fmt.Println(getValueFromMap("valueOne"))
+	fmt.Println(getValueFromMap("valueTwo"))
+}
+
+func addValuesToMap() {
+	hashMap["valueOne"] = 1
+	hashMap["valueTwo"] = 2
+}
+
+func getValueFromMap(key string) (result int) {
+	return hashMap[key]
 }
